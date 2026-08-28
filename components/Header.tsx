cfda00110409f79b7d8fd6ae307b8af5413217da
novaeeken <nova_eeken@hotmail.com>
2026-08-28 16:57:20 +0200

    Added idea folder to gitignore

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import Image, { ImageProps } from "next/image";
+
+type HeaderProps = {
+    icon: ImageProps["src"];
+    title: string;
+};
+
+export default function Header({ icon, title }: HeaderProps) {
+    return (
+        <header className="header">
+            <Image
+                className="header-icon"
+                src={icon}
+                alt="header-icon"
+                width={60}
+                height={60}
+            />
+
+            <h1>{title}</h1>
+        </header>
+    );
+}
